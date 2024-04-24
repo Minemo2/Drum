ce78fa7ea6d79ae526ece38ce756b77d32b81611
Minemo2 <167322229+Minemo2@users.noreply.github.com>
2024-04-24 16:47:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+// For loop for adding listener to our all 7 buttons
+/*for (var i = 0; i<=numberOfDrumButtons; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+}*/
+
+// while loop for adding listener to our all 7 buttons
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+});
+
+var j = 0;
+while (j <=numberOfDrumButtons){
+    // call function with mouse click
+    document.querySelectorAll("button")[j].addEventListener("click", function(){
+        var buttonValue = this.innerHTML;
+        makeSound(buttonValue);
+    });
+    // call function with key press
+    //document.querySelectorAll("button")[j].addEventListener("keydown", handleClick);
+    j++;
+}
+
+
+function makeSound(key){
+    // switch statement for playing different sounds
+    switch(key){
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        //default: console.log(buttonValue);
+    }
+}
